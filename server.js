2b7ee832bc86dbbbba274c731b976b8f11b1a9ee
basma skaik <basmahskaik@gmail.com>
2024-12-17 18:19:56 +0200

    build the structure

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded());
+
+app.use(
+  cookieSession({
+    name: "basma-session",
+    keys: [process.env.COOKIE_SECRET],
+    httpOnly: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Cloud-Based Healthcare Application.",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
